ca5e69b686d1237fb73230d6d890d74ab5774e6c
S.A <sodiqabdulwaris@gmail.com>
2026-05-31 23:49:57 +0100

    deployment

--- backend/src/index.js
+++ backend/src/index.js
@@ -14,9 +14,9 @@ const errorHandler = require('./middlewares/error.middleware');
 const app = express();
 const PORT = config.port || 5000;
 
-// CORS — must come before routes
+// CORS
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: process.env.FRONTEND_URL || 'http://localhost:5173',
   credentials: true,
 }));
 
